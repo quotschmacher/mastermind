02cb38a422ba26665cbdfd45bb0a11a51ec47790
Sven Kicherer <sven.kicherer@yahoo.com>
2021-08-26 23:09:39 +0200

    added LED-test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define LED_PIN     2
+#define LED_COUNT   36
+
+CRGB leds[LED_COUNT];
+
+void setup()
+{
+    // put your setup code here, to run once:
+    delay(3000);
+    FastLED.addLeds<SK6812, LED_PIN, RGB>(leds, LED_COUNT);
+    FastLED.setBrightness(50);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+uint8_t actual_led = 0;
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    //fill_rainbow();
+    // alle LEDS auf schwarz
+    for (int i = 0; i < LED_COUNT; i++)
+    {
+        leds[i] = CRGB::Black;
+    }
+    // eine LED auf blau
+    leds[actual_led] = CRGB::Blue;
+    // dann eine LED weitergehen, ggf zum Anfang
+    if (++actual_led >= LED_COUNT)
+    {
+        actual_led = 0;
+    }
+    FastLED.show();
+    FastLED.delay(500);
+    //EVERY_N_MILLISECONDS(50) { fill_rainbow(leds, LED_COUNT); }
 }
